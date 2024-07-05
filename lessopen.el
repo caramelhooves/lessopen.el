f982c04b1e950e2d3c26417014b54a24f91e88f1
Caramel Hooves <caramel.hooves@protonmail.com>
2024-07-05 20:54:31 +0200

    fix: checkdoc

--- lessopen.el
+++ lessopen.el
@@ -23,6 +23,7 @@
 (require 'view)
 
 (defun lessopen--sentinel (process event)
+  "Hangle changet in PROCESS status. Pass EVENT to callback registered in `lessopen--open'."
   (let ((done-cb (process-get process :done-cb)))
     (cond
      ((string-match-p "\\(exited abnormally.*\\|finished\\)" event)
@@ -34,6 +35,7 @@
      (t (message "process %s changed status %s" process event)))))
 
 (defun lessopen--insert-into-buffer (process output)
+  "Insert PROCESS's OUTPUT in the read-only process buffer."
   (with-current-buffer (process-buffer process)
     (let ((inhibit-read-only t))
       (save-excursion
@@ -43,7 +45,7 @@
     )))
 
 (defun lessopen--open (filename &optional done-cb)
-  "Open FILENAME in view-mode. pre-process the file content using LESSOPEN/LESSCLOSE into the current buffer. Call DONE-CB when parsing is done."
+  "Open FILENAME in `view-mode'. pre-process the file content using LESSOPEN/LESSCLOSE into the current buffer. Call DONE-CB when parsing is done."
   (let ((lessopen (getenv "LESSOPEN"))
         (done-cb (or done-cb (lambda (&rest _args) (message "lessopen: pre-processing %s done" (file-name-nondirectory filename))))))
     (cond
@@ -59,7 +61,7 @@
         (process-put process :done-cb done-cb))))))
 
 (defun view-file-with-lessopen (filename)
-  "Open FILENAME in view-mode, pre-process the file content using LESSOPEN/LESSCLOSE."
+  "Open FILENAME in `view-mode', pre-process the file content using LESSOPEN/LESSCLOSE."
    (interactive "f")
    (let ((filename (file-truename filename))
          (buf (generate-new-buffer filename)))
